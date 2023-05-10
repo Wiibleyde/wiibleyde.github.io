863a42f5e2c5ade62a98f6100563f8a596b18a2f
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-05-10 19:38:06 +0200

    Optimization
    Signed-off-by: Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>

--- jostophe/static/script.js
+++ jostophe/static/script.js
@@ -1,51 +1,42 @@
-const oiRegex = /oi/gi
-const estRegex = /est/gi
-const etRegex = /et/gi
-const esRegex = /\bes\b/gi
-const comRegex = /com/gi
-const çRegex = /ç/gi
-const ienRegex = /ien/gi
-const aiRegex = /ai/gi
-const ffRegex = /ff/gi
-const mmRegex = /mm/gi
-const quRegex = /qu/gi
-const éRegex = /é/gi
-const ccRegex = /cc/gi
-const ceRegex = /ce/gi
-const perRegex = /per/gi
-const ezRegex = /ez/gi
-const finalTRegex = /t\b(?!\s+(?:Fleet|Constant)\b)/gi
-const finalErRegex = /er\b(?!\s+(?:rien)\b)/gi
-const finalSionRegex = /sion\b(?!\s+(?:NS)\b)/gi
-const finalTionRegex = /tion\b(?!\s+(?:rien)\b)/gi
-
+const regexs = [
+    { oiRegex: /oi/gi, oiReplace: "oa" },
+    { estRegex: /est/gi, estReplace: "ai" },
+    { etRegex: /et/gi, etReplace: "é" },
+    { esRegex: /\bes\b/gi, esReplace: "ai" },
+    { comRegex: /com/gi, comReplace: "kom" },
+    { çRegex: /ç/gi, çReplace: "s" },
+    { ienRegex: /ien/gi, ienReplace: "i1" },
+    { aiRegex: /ai/gi, aiReplace: "é" },
+    { ffRegex: /ff/gi, ffReplace: "f" },
+    { mmRegex: /mm/gi, mmReplace: "m" },
+    { quRegex: /qu/gi, quReplace: "k" },
+    { éRegex: /é/gi, éReplace: "ai" },
+    { ccRegex: /cc/gi, ccReplace: "k" },
+    { ceRegex: /ce/gi, ceReplace: "se" },
+    { perRegex: /per/gi, perReplace: "père" },
+    { ezRegex: /ez/gi, ezReplace: "é" },
+    { finalTRegex: /t\b(?!\s+(?:Fleet|Constant)\b)/gi, finalTReplace: "" },
+    { finalErRegex: /er\b(?!\s+(?:rien)\b)/gi, finalErReplace: "é" },
+    { finalSionRegex: /sion\b(?!\s+(?:NS)\b)/gi, finalSionReplace: "tion" },
+    { finalTionRegex: /tion\b(?!\s+(?:rien)\b)/gi, finalTionReplace: "sion" },
+]
+  
 function translateJostophe(sourceId, resultId) {
     let result = document.getElementById(resultId)
     result.innerHTML = "Processing..."
     let textarea = document.getElementById(sourceId)
     let text = textarea.value
-    let translatedText = text.replace(oiRegex, "oa")
-    translatedText = translatedText.replace(éRegex,"ai").replace(aiRegex,"é")
-    translatedText = translatedText.replace(estRegex, "ai")
-    translatedText = translatedText.replace(etRegex, "é")
-    translatedText = translatedText.replace(esRegex,"ai")
-    translatedText = translatedText.replace(ezRegex,"é")
-    translatedText = translatedText.replace(quRegex,"k")
-    translatedText = translatedText.replace(comRegex,"kom")
-    translatedText = translatedText.replace(ccRegex,"k")
-    translatedText = translatedText.replace(ceRegex,"se")
-    translatedText = translatedText.replace(perRegex,"père")
-    translatedText = translatedText.replace(çRegex,"s")
-    translatedText = translatedText.replace(ienRegex,"i1")
-    translatedText = translatedText.replace(ffRegex, "f")
-    translatedText = translatedText.replace(mmRegex, "m")
-    translatedText = translatedText.replace(finalTRegex,"")
-    // translatedText = translatedText.replace(finalSRegex,"")
-    translatedText = translatedText.replace(finalErRegex,"é")
-    translatedText = translatedText.replace(finalSionRegex,"tion")
-    translatedText = translatedText.replace(finalTionRegex,"sion")
+    let translatedText = text
+  
+    for (let i = 0; i < regexs.length; i++) {
+        let regex = regexs[i]
+        translatedText = translatedText.replace(regex[Object.keys(regex)[0]], regex[Object.keys(regex)[1]])
+    }
+  
     result.innerHTML = translatedText;
 }
+  
+
 
 function copyText(textareaId) {
     let textarea = document.getElementById(textareaId)
