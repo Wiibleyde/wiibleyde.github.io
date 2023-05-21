c1c16d72e242bd524bbd55df19a19c8ff5dcf8c6
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-05-21 16:54:49 +0200

    Little modification of WaitingScreen
    Signed-off-by: Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>

--- WaitingScreen/script.js
+++ WaitingScreen/script.js
@@ -1,49 +1,71 @@
 document.addEventListener("DOMContentLoaded", function () {
-	loopAnimation()
-})
+	loopAnimation();
+});
 
-const textParam = new URLSearchParams("text")
+const textParam = new URLSearchParams("text");
 
 function animation_text_1(element) {
-	var newText = ""
-	var theText = document.querySelector(element)
+	var newText = "";
+	var theText = document.querySelector(element);
 	for (i = 0; i < theText.innerText.length; i++) {
-		newText += "<div>"
-		if (theText.innerText[i] == " ") { newText += "&nbsp" }
-		else { newText += theText.innerText[i] }
-		newText += "</div>"
+		newText += "<div>";
+		if (theText.innerText[i] == " ") {
+			newText += "&nbsp";
+		} else {
+			newText += theText.innerText[i];
+		}
+		newText += "</div>";
 	}
-	theText.innerHTML = newText
-	var targetsDiv = document.querySelectorAll(element + " div")
-	TweenMax.staggerFromTo(targetsDiv, 2, { opacity: 0, y: 90, ease: Elastic.easeOut.config(1.2, 0.5) }, { opacity: 1, y: 0, ease: Elastic.easeOut.config(1.2, 0.5) }, 0.03)
+	theText.innerHTML = newText;
+	var targetsDiv = document.querySelectorAll(element + " div");
+	TweenMax.staggerFromTo(
+		targetsDiv,
+		2,
+		{ opacity: 0, y: 90, ease: Elastic.easeOut.config(1.2, 0.5) },
+		{ opacity: 1, y: 0, ease: Elastic.easeOut.config(1.2, 0.5) },
+		0.03
+	);
 }
 
 function unanimation_text_1(element) {
-	var newText = ""
-	var theText = document.querySelector(element)
+	var newText = "";
+	var theText = document.querySelector(element);
 	for (i = 0; i < theText.innerText.length; i++) {
-		newText += "<div>"
-		if (theText.innerText[i] == " ") { newText += "&nbsp" }
-		else { newText += theText.innerText[i] }
-		newText += "</div>"
+		newText += "<div>";
+		if (theText.innerText[i] == " ") {
+			newText += "&nbsp";
+		} else {
+			newText += theText.innerText[i];
+		}
+		newText += "</div>";
 	}
-	theText.innerHTML = newText
-	var targetsDiv = document.querySelectorAll(element + " div")
-	TweenMax.staggerFromTo(targetsDiv, 2, { opacity: 1, y: 0, ease: Elastic.easeOut.config(1.2, 0.5) }, { opacity: 0, y: 90, ease: Elastic.easeOut.config(1.2, 0.5) }, 0.03)
+	theText.innerHTML = newText;
+	var targetsDiv = document.querySelectorAll(element + " div");
+	TweenMax.staggerFromTo(
+		targetsDiv,
+		2,
+		{ opacity: 1, y: 0, ease: Elastic.easeOut.config(1.2, 0.5) },
+		{ opacity: 0, y: 90, ease: Elastic.easeOut.config(1.2, 0.5) },
+		0.03
+	);
 }
 
 function loopAnimation() {
-	animation_text_1("#text-anim")
-	setTimeout(function () { unanimation_text_1("#text-anim") }, 2000)
-	setTimeout(function () { loopAnimation() }, 4000)
+	animation_text_1("#text-anim");
+	setTimeout(function () {
+		unanimation_text_1("#text-anim");
+	}, 2000); // Delay before unanimation_text_1() is called
+	setTimeout(function () {
+		loopAnimation();
+	}, 4000); // Delay before restarting the loop
 }
 
 function getUrlParam(paramName) {
-	let params = new URLSearchParams(location.search)
-	return params.get(paramName)
+	let params = new URLSearchParams(location.search);
+	return params.get(paramName);
 }
 
-text = getUrlParam("text")
+text = getUrlParam("text");
 if (text) {
-	document.querySelector("#text-anim").innerText = text
-} 
+	document.querySelector("#text-anim").innerText = text;
+}
