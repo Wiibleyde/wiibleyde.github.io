a1e0c9bdc764aa3c0aedfb0a5a523ac3c22b9606
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-06-22 18:46:02 +0200

    Himitsu Update
    Signed-off-by: Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>

--- himitsu/script.js
+++ himitsu/script.js
@@ -1,42 +1,48 @@
-(function () {
-  const second = 1000,
-    minute = second * 60,
-    hour = minute * 60,
-    day = hour * 24;
-  let today = new Date(),
-    dd = String(today.getDate()).padStart(2, "0"),
-    mm = String(today.getMonth() + 1).padStart(2, "0"),
-    yyyy = today.getFullYear(),
-    nextYear = yyyy + 1,
-    dayMonth = "06/24/",
-    birthday = dayMonth + yyyy;
+function timer(year, month, day, hour, minute, second) {
+    let targetDate = new Date(year, month - 1, day, hour, minute, second)
+    let currentDate = new Date()
 
-  today = mm + "/" + dd + "/" + yyyy;
-  if (today > birthday) {
-    birthday = dayMonth + nextYear;
-  }
-  const countDown = new Date(birthday).getTime(),
-    x = setInterval(function () {
+    let timeRemaining = targetDate - currentDate
 
-      const now = new Date().getTime(),
-        distance = countDown - now;
+    if (timeRemaining <= 0) {
+        return true
+    } else {
+        let days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24))
+        let hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60))
+        let seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000)
 
-      document.getElementById("days").innerText = Math.floor(distance / (day)),
-        document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+        document.getElementById('days').textContent = days
+        document.getElementById('hours').textContent = hours
+        document.getElementById('minutes').textContent = minutes
+        document.getElementById('seconds').textContent = seconds
 
-      if (distance < 0) {
-        document.getElementById("headline").innerText = "Himitsu arrive très bientôt !";
-        document.getElementById("countdown").style.display = "none";
-        document.getElementById("content").style.display = "block";
-        document.getElementById("background").style.backgroundImage = "url(./newbg.jpg)";
-        document.getElementById("background").style.backgroundSize = "cover";
-        clearInterval(x);
-      } else {
-        document.getElementById("headline").innerText = "Temps avant l'arrivée d'Himitsu";
-        document.getElementById("background").style.backgroundImage = "url(./himitsu.jpg)";
-        document.getElementById("background").style.backgroundSize = "cover";
-      }
-    }, 0)
-}());
+        return false
+    }
+}
+
+function updateTimer() {
+    let year = 2023
+    let month = 6
+    let day = 24
+    let hour = 14
+    let minute = 30
+    let second = 0
+
+    let timerFinished = timer(year, month, day, hour, minute, second)
+
+    if (timerFinished) {
+        document.getElementById("headline").innerText = "Himitsu arrive très bientôt !"
+        document.getElementById("countdown").style.display = "none"
+        document.getElementById("content").style.display = "block"
+        document.getElementById("background").style.backgroundImage = "url(./newbg.jpg)"
+        document.getElementById("background").style.backgroundSize = "cover"
+    } else {
+        document.getElementById("headline").innerText = "Temps avant l'arrivée d'Himitsu"
+        document.getElementById("background").style.backgroundImage = "url(./himitsu.jpg)"
+        document.getElementById("background").style.backgroundSize = "cover"
+        setTimeout(updateTimer, 100)
+    }
+}
+
+updateTimer()
