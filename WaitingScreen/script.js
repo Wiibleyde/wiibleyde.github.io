4f0c52786cf5acfee6ed2ad328ac692b119aac38
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-08-07 18:27:58 +0200

    Optimize WaitingScreen
    Signed-off-by: Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>

--- WaitingScreen/script.js
+++ WaitingScreen/script.js
@@ -1,71 +1,50 @@
-document.addEventListener("DOMContentLoaded", function () {
-	loopAnimation();
-});
-
 const textParam = new URLSearchParams("text");
 
-function animation_text_1(element) {
-	var newText = "";
-	var theText = document.querySelector(element);
-	for (i = 0; i < theText.innerText.length; i++) {
-		newText += "<div>";
-		if (theText.innerText[i] == " ") {
-			newText += "&nbsp";
-		} else {
-			newText += theText.innerText[i];
-		}
-		newText += "</div>";
-	}
-	theText.innerHTML = newText;
-	var targetsDiv = document.querySelectorAll(element + " div");
-	TweenMax.staggerFromTo(
-		targetsDiv,
-		2,
-		{ opacity: 0, y: 90, ease: Elastic.easeOut.config(1.2, 0.5) },
-		{ opacity: 1, y: 0, ease: Elastic.easeOut.config(1.2, 0.5) },
-		0.03
-	);
-}
+function processTextAnimation(element, unanimate = false) {
+	const targetElement = document.querySelector(element);
+	const text = targetElement.innerText;
+	const charArray = Array.from(text);
 
-function unanimation_text_1(element) {
-	var newText = "";
-	var theText = document.querySelector(element);
-	for (i = 0; i < theText.innerText.length; i++) {
-		newText += "<div>";
-		if (theText.innerText[i] == " ") {
-			newText += "&nbsp";
-		} else {
-			newText += theText.innerText[i];
-		}
-		newText += "</div>";
-	}
-	theText.innerHTML = newText;
-	var targetsDiv = document.querySelectorAll(element + " div");
-	TweenMax.staggerFromTo(
-		targetsDiv,
-		2,
-		{ opacity: 1, y: 0, ease: Elastic.easeOut.config(1.2, 0.5) },
-		{ opacity: 0, y: 90, ease: Elastic.easeOut.config(1.2, 0.5) },
-		0.03
-	);
+	const newText = charArray
+		.map(char => {
+			if (char === ' ') {
+				return '&nbsp;';
+			}
+			return `<div>${char}</div>`;
+		})
+		.join('');
+
+	targetElement.innerHTML = newText;
+
+	const targetsDiv = targetElement.querySelectorAll('div');
+	const animationConfig = {
+		opacity: unanimate ? 0 : 1,
+		y: unanimate ? 90 : 0,
+		ease: Elastic.easeOut.config(1.2, 0.5)
+	};
+
+	TweenMax.staggerFromTo(targetsDiv, 2, animationConfig, { opacity: 1 - animationConfig.opacity, y: 90 - animationConfig.y, ease: Elastic.easeOut.config(1.2, 0.5) }, 0.03);
 }
 
 function loopAnimation() {
-	animation_text_1("#text-anim");
-	setTimeout(function () {
-		unanimation_text_1("#text-anim");
-	}, 2000); // Delay before unanimation_text_1() is called
-	setTimeout(function () {
-		loopAnimation();
-	}, 4000); // Delay before restarting the loop
+	const textElement = "#text-anim";
+	processTextAnimation(textElement);
+	setTimeout(() => {
+		processTextAnimation(textElement, true);
+		setTimeout(loopAnimation, 3000);
+	}, 3000);
 }
 
 function getUrlParam(paramName) {
-	let params = new URLSearchParams(location.search);
+	const params = new URLSearchParams(location.search);
 	return params.get(paramName);
 }
 
-text = getUrlParam("text");
+const text = getUrlParam("text");
 if (text) {
 	document.querySelector("#text-anim").innerText = text;
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+	loopAnimation();
+});
