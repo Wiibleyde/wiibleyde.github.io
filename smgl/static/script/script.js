19e9fbd80dd2df15ee72474268d604c2d85da8ec
Wiibleyde <nathan@bonnell.fr>
2024-01-19 11:51:33 +0100

    Add countdown timer and update year in index.html and script.js
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- smgl/static/script/script.js
+++ smgl/static/script/script.js
@@ -52,6 +52,29 @@ function randomPos(id) {
     console.log("Position: " + random1sqrt + "%, " + random2sqrt + "%");
 }
 
+const countdown = new Date("Sep 1, 2024 00:00:00").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countdown - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) /
+        (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) /
+        (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("countdown-days").innerHTML = days;
+    document.getElementById("countdown-hours").innerHTML = hours;
+    document.getElementById("countdown-minutes").innerHTML = minutes;
+    document.getElementById("countdown-seconds").innerHTML = seconds;
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown-days").innerHTML = "0";
+        document.getElementById("countdown-hours").innerHTML = "0";
+        document.getElementById("countdown-minutes").innerHTML = "0";
+        document.getElementById("countdown-seconds").innerHTML = "0";
+    }
+}, 10);
+
 randomPos("randomPos")
 
 lazyLoadVideo(videoElement);
